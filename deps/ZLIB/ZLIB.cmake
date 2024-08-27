8c815df592b3c673ebc229c14ac73a414b048980
Henri van der Riet <henrivdr@gmail.com>
2024-08-27 20:16:55 +0200

    bump zlib to version 1.3

--- deps/ZLIB/ZLIB.cmake
+++ deps/ZLIB/ZLIB.cmake
@@ -3,10 +3,12 @@ set(patch_command git init && ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/0001-Respec
 orcaslicer_add_cmake_project(ZLIB
   # GIT_REPOSITORY https://github.com/madler/zlib.git
   # GIT_TAG v1.2.11
-  #URL https://github.com/madler/zlib/archive/refs/tags/v1.2.11.zip
-  URL https://github.com/madler/zlib/archive/refs/tags/v1.2.13.zip
-  #URL_HASH SHA256=f5cc4ab910db99b2bdbba39ebbdc225ffc2aa04b4057bc2817f1b94b6978cfc3
-  URL_HASH SHA256=c2856951bbf30e30861ace3765595d86ba13f2cf01279d901f6c62258c57f4ff
+  # URL https://github.com/madler/zlib/archive/refs/tags/v1.2.11.zip
+  # URL_HASH SHA256=f5cc4ab910db99b2bdbba39ebbdc225ffc2aa04b4057bc2817f1b94b6978cfc3
+  # URL https://github.com/madler/zlib/archive/refs/tags/v1.2.13.zip
+  # URL_HASH SHA256=c2856951bbf30e30861ace3765595d86ba13f2cf01279d901f6c62258c57f4ff
+  URL https://github.com/madler/zlib/releases/download/v1.3/zlib13.zip
+  URL_HASH SHA256=c561d09347f674f0d72692e7c75d9898919326c532aab7f8c07bb43b07efeb38  
   PATCH_COMMAND ${patch_command}
   CMAKE_ARGS
     -DSKIP_INSTALL_FILES=ON         # Prevent installation of man pages et al.
