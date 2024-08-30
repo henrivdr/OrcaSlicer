d752d4e473b5e08c724ddd813d5592a165df600d
Henri van der Riet <henrivdr@gmail.com>
2024-08-30 20:44:42 +0200

    bump qhull to v8.0.2

--- deps/Qhull/Qhull.cmake
+++ deps/Qhull/Qhull.cmake
@@ -2,9 +2,9 @@ include(GNUInstallDirs)
 orcaslicer_add_cmake_project(Qhull
     # URL "https://github.com/qhull/qhull/archive/v8.0.1.zip"
     # URL_HASH SHA256=5287f5edd6a0372588f5d6640799086a4033d89d19711023ef8229dd9301d69b
-    URL "https://github.com/qhull/qhull/archive/refs/tags/v8.1-alpha3.zip"
-    URL_HASH SHA256=7bd9b5ffae01e69c2ead52f9a9b688af6c65f9a1da05da0a170fa20d81404c06    
-    CMAKE_ARGS 
+    GIT_REPOSITORY https://github.com/qhull/qhull.git
+    GIT_TAG        v8.0.2
+    CMAKE_ARGS
         -DINCLUDE_INSTALL_DIR=${CMAKE_INSTALL_INCLUDEDIR}
 )
 
